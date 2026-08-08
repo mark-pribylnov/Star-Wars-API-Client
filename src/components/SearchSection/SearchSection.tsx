fbb69f3d071e5239ddbc9b31a28d94c4fa81c59e
mark-pribylnov <mark.pribylnov@gmail.com>
2026-08-08 21:10:44 +0300

    feat: add saving and restoring search term from local storage

--- src/components/SearchSection/SearchSection.tsx
+++ src/components/SearchSection/SearchSection.tsx
@@ -1,8 +1,27 @@
-import { Component } from 'react';
+import { Component, type SubmitEvent } from 'react';
+// import ApiService from '../../services/api';
 import styles from './SearchSection.module.scss';
 import SearchIcon from '../../assets/icons/search/SearchIcon';
 
 export default class SearchSection extends Component {
+  // private api = new ApiService();
+  private inputName = 'searchTerm';
+  private initialSearchTerm = localStorage.getItem(this.inputName);
+
+  private handleSubmit = (e: SubmitEvent) => {
+    e.preventDefault();
+
+    if (!(e.currentTarget instanceof HTMLFormElement))
+      throw new Error('e.currentTarget is not HTMLFormElement');
+
+    const formData = new FormData(e.currentTarget);
+    const searchTerm = formData.get(this.inputName);
+    if (typeof searchTerm !== 'string')
+      throw new Error('Search term is not a string');
+
+    localStorage.setItem(this.inputName, searchTerm);
+  };
+
   render() {
     return (
       <div className={styles.root}>
@@ -11,20 +30,22 @@ export default class SearchSection extends Component {
           Search the galaxy
         </h2>
 
-        <form action="#" className={styles['search-form']}>
+        <form
+          action="#"
+          className={styles['search-form']}
+          onSubmit={this.handleSubmit}
+        >
           <input
             type="text"
+            name={this.inputName}
             className={styles['search-input']}
             placeholder="Type to search..."
+            defaultValue={this.initialSearchTerm ?? ''}
           />
           <button type="submit" className={styles['search-button']}>
             Search
           </button>
         </form>
-
-        <p className={styles['search-description']}>
-          Search for characters, planets, starships, species and films...
-        </p>
       </div>
     );
   }
---
+++ src/services/api.ts
@@ -0,0 +1,5 @@
+export default class ApiService {
+  search(searchTerm: string) {
+    console.log(searchTerm);
+  }
+}
