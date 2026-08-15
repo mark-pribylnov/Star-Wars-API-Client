4542b3e2f5e4512b96981a7a87b82cd28a8fd0ea
mark-pribylnov <mark.pribylnov@gmail.com>
2026-08-15 14:19:03 +0300

    refactor: turn App.tsx into class component

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,20 @@
 // TODO: in types 'additional property: false' is duplicated in each file
 // TODO: show HTTP error to the user, not console
 
+import { Component } from 'react';
 import styles from './App.module.scss';
 import SearchSection from './components/SearchSection/SearchSection';
 import ResultsSection from './components/ResultsSection/ResultsSection';
 import { Header } from './components/Header/Header';
 
-function App() {
-  return (
-    <div className={styles['app-wrapper']}>
-      <Header />
-      <SearchSection />
-      <ResultsSection resultsNumber={5} />
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className={styles['app-wrapper']}>
+        <Header />
+        <SearchSection />
+        <ResultsSection resultsNumber={5} />
+      </div>
+    );
+  }
 }
-
-export default App;
