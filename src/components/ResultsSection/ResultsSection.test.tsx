1a00f46722fc184eb84a4b5021e40bfd3097d88f
mark-pribylnov <mark.pribylnov@gmail.com>
2026-09-16 08:43:55 +0300

    test: add tests for ResultsSection when no results found

--- src/components/ResultsSection/ResultsSection.test.tsx
+++ src/components/ResultsSection/ResultsSection.test.tsx
@@ -4,45 +4,91 @@ import ResultsSection from './ResultsSection';
 import { mockApiData } from '../../test/fixtures/mockApiData';
 import { unpackData } from '../../utils/utils';
 
-describe('Results section without submitting a search', () => {
-  const data = unpackData(mockApiData);
+describe('Results section', () => {
+  describe('With results (submitted or empty search)', () => {
+    const data = unpackData(mockApiData);
 
-  beforeEach(() => {
-    render(
-      <ResultsSection
-        searchResults={data}
-        searchTerm={null}
-        isLoading={false}
-        loadError={null}
-        onRetryLoadData={() => undefined}
-      />
-    );
+    beforeEach(() => {
+      render(
+        <ResultsSection
+          searchResults={data}
+          searchTerm={null}
+          isLoading={false}
+          loadError={null}
+          onRetryLoadData={() => undefined}
+        />
+      );
+    });
+
+    test('Heading with the number is visible', () => {
+      expect(
+        screen.getByRole('heading', {
+          level: 2,
+          name: /^search results\s*\d+$/i,
+        })
+      ).toBeVisible();
+    });
+
+    test('Item image, name and description are visible', () => {
+      const firstItemData = data[0];
+      expect(
+        screen.getByRole('img', { name: firstItemData.name })
+      ).toBeVisible();
+      expect(screen.getByText(firstItemData.name)).toBeVisible();
+      expect(screen.getByText(firstItemData.description)).toBeVisible();
+    });
+
+    test('Column headers name and description are visible', () => {
+      expect(
+        screen.getByRole('columnheader', {
+          name: /name/i,
+        })
+      ).toBeVisible();
+
+      expect(
+        screen.getByRole('columnheader', {
+          name: /description/i,
+        })
+      ).toBeVisible();
+    });
   });
 
-  test('Heading with the number is visible', () => {
-    expect(
-      screen.getByRole('heading', { level: 2, name: /^search results\s*\d+$/i })
-    ).toBeVisible();
-  });
+  describe('Without search results', () => {
+    const searchTerm = 'Luke Groundwalker';
 
-  test('Item image, name and description are visible', () => {
-    const firstItemData = data[0];
-    expect(screen.getByRole('img', { name: firstItemData.name })).toBeVisible();
-    expect(screen.getByText(firstItemData.name)).toBeVisible();
-    expect(screen.getByText(firstItemData.description)).toBeVisible();
-  });
+    beforeEach(() => {
+      render(
+        <ResultsSection
+          searchResults={[]}
+          searchTerm={searchTerm}
+          isLoading={false}
+          loadError={null}
+          onRetryLoadData={() => undefined}
+        />
+      );
+    });
 
-  test('Column headers name and description are visible', () => {
-    expect(
-      screen.getByRole('columnheader', {
-        name: /name/i,
-      })
-    ).toBeVisible();
+    test('Heading says "No results found"', () => {
+      expect(
+        screen.getByRole('heading', {
+          level: 2,
+          name: /no results found/i,
+        })
+      ).toBeVisible();
+    });
 
-    expect(
-      screen.getByRole('columnheader', {
-        name: /description/i,
-      })
-    ).toBeVisible();
+    test('Description includes the search term', () => {
+      expect(screen.getByText(searchTerm)).toBeVisible();
+    });
+
+    test('The image is visible', () => {
+      expect(
+        screen.getByRole('img', { name: 'no results found' })
+      ).toBeVisible();
+    });
+
+    test('The quote below image is visible', () => {
+      expect(screen.getByText('— YODA')).toBeVisible();
+    });
   });
 });
