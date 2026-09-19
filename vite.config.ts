2f7f79af0b6190f4aef90a32bc58f75050520337
mark-pribylnov <mark.pribylnov@gmail.com>
2026-09-19 19:10:30 +0300

    chore: update test coverage thresholds and modify test command in CI

--- vite.config.ts
+++ vite.config.ts
@@ -19,10 +19,10 @@ export default defineConfig({
         'src/main.tsx',
       ],
       thresholds: {
-        lines: 80,
-        functions: 80,
-        branches: 80,
         statements: 80,
+        branches: 50,
+        functions: 50,
+        lines: 50,
       },
     },
   },
